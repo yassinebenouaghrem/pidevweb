f40d39eb90fbf5f2e94d39af4d05cfa3b7d2f399
eskander1998 <47850204+eskander1998@users.noreply.github.com>
2021-04-25 23:29:11 +0100

    Add files via upload

--- src/Repository/EvenementRepository.php
+++ src/Repository/EvenementRepository.php
@@ -83,7 +83,7 @@ public function FindEventEncours(FitreRecherche $search)
 
         $query=$entityManager
             ->createQuery("select r from App\Entity\ReservationEvent r where 
-                      r.etat LIKE :etat and ((DATE_DIFF( r.dateEvent,CURRENT_DATE()) )>=0)")
+                      r.etat LIKE :etat and ((DATE_DIFF( r.dateEvent,CURRENT_DATE()) )>=0) order by r.id desc")
             ->setParameter('etat', 'en cours');
         /*
                         $query2=$entityManager
@@ -225,8 +225,15 @@ public function FindEventEncours(FitreRecherche $search)
         */
 
 
-        return $query->getResult() ;
+       // return $query->getResult() ;
 
+        return $this->getDoctrine()
+            ->getRepository(EvenementRepository::class)
+            ->createQueryBuilder('e')
+            ->addOrderBy('e.id', 'desc')
+            ->setMaxResults(4)
+            ->getQuery()
+            ->execute();
     }
 
     public function FindReservation()
@@ -247,7 +254,7 @@ public function FindEventEncours(FitreRecherche $search)
         return $query->getResult() ;
 
     }
-    public function AfficherEvenementencours()
+    public function AfficherEvenementencours(FitreRecherche $search)
     {
         $entityManager= $this->getEntityManager();
 
@@ -260,6 +267,57 @@ public function FindEventEncours(FitreRecherche $search)
                             ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
                             ");
         */
+
+        if ($search->getPrixMax()){
+            $query=$entityManager
+                ->createQuery('select e from App\Entity\Evenement e where 
+                      e.tarif <= :PrixMax')
+                ->setParameter('PrixMax',  $search->getPrixMax());
+
+        }
+        if ($search->getTypeEvent()){
+
+            $query=$entityManager
+                ->createQuery('select e from App\Entity\Evenement e where 
+                      e.type = :TypeEvent')
+                ->setParameter('TypeEvent', $search->getTypeEvent());
+        }
+
+
+
+        if ($search->getTypeEvent() && $search->getPrixMax()){
+
+            $query=$entityManager
+                ->createQuery('select e from App\Entity\Evenement e where 
+                      e.type = :TypeEvent and  
+                      e.tarif <= :PrixMax ')
+                ->setParameter('TypeEvent', $search->getTypeEvent())
+                ->setParameter('PrixMax', $search->getPrixMax());
+
+        }
+
+        if ($search->getDateDebut() && $search->getDateFin() && $search->getTypeEvent() && $search->getPrixMax()){
+
+            $query=$entityManager
+                ->createQuery('select e from App\Entity\Evenement e where 
+                      ((e.dateEvent between :datedebut and :datefin) and e.type = :TypeEvent and  
+                      e.tarif <= :PrixMax )')
+                ->setParameter('datedebut', $search->getDateDebut())
+                ->setParameter('datefin', $search->getDateFin())
+                ->setParameter('TypeEvent', $search->getTypeEvent())
+                ->setParameter('PrixMax', $search->getPrixMax());
+
+        }
+
+        if ($search->getDateDebut() && $search->getDateFin()){
+
+            $query=$entityManager
+                ->createQuery('select e from App\Entity\Evenement e where 
+                      (e.dateEvent between :datedebut and :datefin)')
+                ->setParameter('datedebut', $search->getDateDebut())
+                ->setParameter('datefin', $search->getDateFin());
+
+        }
 
 
         return $query->getResult() ;
@@ -274,6 +332,169 @@ public function FindEventEncours(FitreRecherche $search)
             ->createQuery("select e from App\Entity\Evenement e where 
                      (e.etat=:etat)")
             ->setParameter('etat', 'effectue');
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function detailReservationEvent(int $idEvent)
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("select e from App\Entity\ReservationEvent e where 
+                     (e.idEvent=:idEvent)")
+            ->setParameter('idEvent', $idEvent);
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+
+    public function StatTypeEventleplusreserver()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("SELECT sum(e.nbReservation) as reserv,e.type  FROM App\Entity\Evenement e group by e.type ");
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function modePaiementleplusUtiliser()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("SELECT r.modePaiement,count(r.modePaiement) as countM FROM App\Entity\ReservationEvent r  group by r.modePaiement ");
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function StatEvolutionReservation()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("SELECT sum(e.nbReservation) as reserve,e.titre  FROM App\Entity\Evenement e where e.etat =:etat group by e.titre  ")
+            ->setParameter('etat', 'en cours');
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function statEtatEvenement()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("SELECT r.etat,count(r.etat) as countEtat FROM App\Entity\Evenement r  group by r.etat ");
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function statProgressionEvenement()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("select e from App\Entity\Evenement e where  (e.etat=:etat)")
+            ->setParameter('etat', 'en cours');
+
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function RappelEvent()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("select e from App\Entity\Evenement e where (DATE_DIFF( e.dateEvent,CURRENT_DATE()) <6) and (e.etat=:etat)")
+            ->setParameter('etat', 'en cours');
+
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function NomDuclient()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("select e from App\Entity\User e ");
+
+
+        /*
+                        $query2=$entityManager
+                            ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
+                            ");
+        */
+
+
+        return $query->getResult() ;
+
+    }
+
+    public function TotalEncaissesurplace()
+    {
+        $entityManager= $this->getEntityManager();
+
+        $query=$entityManager
+            ->createQuery("SELECT sum(total) as totalSurplace,e.type  FROM App\Entity\Evenement e group by e.type ");
         /*
                         $query2=$entityManager
                             ->createQuery("update App\Entity\Evenement e  set e.etat= 'effectue' where ((DATE_DIFF( e.dateEvent,CURRENT_DATE()) )<=0)
